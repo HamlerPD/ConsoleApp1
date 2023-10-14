d085014d652efc1f1a18c8495c3fe4fa8402a2cf
hamle <hamle@DESKTOP-LCJ4CGJ>
2023-10-14 00:10:41 -0400

    Add project files.

---
+++ ConsoleApp1/Miapp1/Cuadrado.cs
@@ -0,0 +1,31 @@
+﻿namespace ConsoleApp1.Miapp1
+{
+
+    public class Cuadrado
+    {
+        private double lado1;
+        private double lado2;
+
+        public Cuadrado()
+        {
+        }
+
+        public Cuadrado(double lado)
+        {
+            lado1 = lado;
+            lado2 = lado;
+
+        }
+        public Cuadrado(double _lado1, double _lado2)
+        {
+            lado1 = _lado1;
+            lado2 = _lado2;
+        }
+        public double perimetro()
+        {
+            return (lado1 + lado2) * 2;
+        }
+    }
+
+
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿using ConsoleApp1.Miapp1;
+
+Cuadrado c;
+c = new Cuadrado();
+Console.WriteLine(" Ingresa el valor del lado1");
+double lado1 = double.Parse(Console.ReadLine());
+Console.WriteLine(" ingresa el valor del lado2");
+double lado2 = double.Parse(Console.ReadLine());
+c = new Cuadrado(lado1, lado2);
+Console.WriteLine(" El perimetro es " +c.perimetro());
